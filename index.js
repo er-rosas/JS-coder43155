6342333f12ed5ed5ba229643c1fbf625b33eb6b1
Ernesto Rosas <er.rosas24@gmail.com>
2023-07-15 17:04:16 -0300

    Primera pre entrega: Simulador interactivo

--- index.js
+++ index.js
@@ -1,34 +1,96 @@
+console.log("¡¡Bienvenido usuario!!")
+
+console.log("Aquí se muetran la información de nuestros productos.")
+class Mercaderia {
+    constructor(producto, precioEnDolar, stock) {
+        this.producto = producto;
+        this.precioEnDolar = precioEnDolar;
+        this.stock = stock;
+    }
+}
+
+mesa = new Mercaderia("Mesa", 30);
+silla = new Mercaderia("Silla", 20);
+tv = new Mercaderia("Tv", 40);
+
+console.log(mesa,silla,tv);
+
+function infoMerc() {
+    return console.log(mesa,silla,tv)
+}
+
+console.log("Escriba comprar()  para iniciar su pedido o infoMerc() para la información de la mercaderia.")
+
 class Producto {
-    constructor(nombre, precio, tacc) {
+    constructor(nombre, precio, impuestos, mesa, silla, tv) {
         this.nombre = nombre;
         this.precio = precio;
-        this.tacc = tacc;
+        this.impuestos = impuestos;
+        this.importeFinal = this.calcularImporteFinal(mesa, silla, tv);
     }
 
-    agregarIva() {
-      return this.precio * 1.21;
-    }
-
-    calcularDescuento() {
-        if (this.tacc == "si") {
-        return this.precio * 0.9;
-        } else {
-        return this.precio;
+    calcularImporteFinal(mesa, silla, tv) {
+        let importeFinal = this.precio + this.impuestos;
+        
+        if (mesa === "si") {
+            importeFinal += 30;
         }
+        if (silla === "si") {
+            importeFinal += 20;
+        }
+        if (tv === "si") {
+            importeFinal += 40;
+        }
+        return importeFinal;
     }
 }
 
-function crearProducto() {
-    let nombre = prompt("Ingrese nombre");
-    let precio = Number(prompt("Ingrese precio"));
-    let tacc = prompt("Es producto sin tacc");
-    let producto = new Producto(nombre, precio, tacc);
-    return producto;
-}
+function comprar() {
+    // Solicitar al usuario los datos del producto
+    let nombre = prompt("Ingrese su nombre:");
+    let precio = parseFloat(prompt("Ingrese el precio de un producto:"));
+    let impuestos = parseFloat(prompt("Ingrese el valor de los impuestos del producto:"));
+    let mesa = prompt("¿Desea comprar una mesa? (si/no):");
+    let silla = prompt("¿Desea comprar una silla? (si/no):");
+    let tv = prompt("¿Desea comprar una TV? (si/no):");
+    let dia = prompt("Día de la semana:")
 
-let i = 1;
-while (i < 4) {
-    let producto = crearProducto();
-    console.log(producto);
-    i++;
+    // Crearamos una nueva instancia de Producto
+    let nuevoProducto = new Producto(nombre, precio, impuestos, mesa, silla, tv, dia);
+
+    // Mostrar el recibo a pagar por el cliente
+    console.log("¡¡Recibo a pagar!!");
+    console.log(nuevoProducto);
+
+    let descuento = 0;
+
+    switch(dia) {
+        case "Lunes":
+            console.log("El descuento del Lunes es: 20%");
+            descuento = nuevoProducto.importeFinal * 0.2;
+            break;
+        case "Martes":
+            console.log("El descuento del Martes es: 10%");
+            descuento = nuevoProducto.importeFinal * 0.1;
+            break;
+        case "Miercoles":
+            console.log("El descuento del Miércoles es: 15%");
+            descuento = nuevoProducto.importeFinal * 0.15;
+            break;
+        case "Jueves":
+            console.log("El descuento del Jueves es: 40%");
+            descuento = nuevoProducto.importeFinal * 0.4;
+            break;
+        case "Viernes":
+            console.log("El descuento del Viernes es: 25%");
+            descuento = nuevoProducto.importeFinal * 0.25;
+            break;
+        default:
+            console.log("El descuento del fin de semana es: 50%");
+            descuento = nuevoProducto.importeFinal * 0.5;
+            break;
+    }
+
+    let importeFinalConDescuento = nuevoProducto.importeFinal - descuento;
+    console.log("Importe final: $" + importeFinalConDescuento);
 }
